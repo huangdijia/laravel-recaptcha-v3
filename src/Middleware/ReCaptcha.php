577c678bdf9e6732ae3af6484f297aa897540264
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2023-07-31 12:25:26 +0800

    Cs fix

--- src/Middleware/ReCaptcha.php
+++ src/Middleware/ReCaptcha.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
  * This file is part of laravel-recaptcha-v3.
  *
  * @link     https://github.com/huangdijia/laravel-recaptcha-v3
- * @document https://github.com/huangdijia/laravel-recaptcha-v3/2.x/main/README.md
+ * @document https://github.com/huangdijia/laravel-recaptcha-v3/blob/2.x/README.md
  * @contact  huangdijia@gmail.com
  */
 namespace Huangdijia\Recaptcha\Middleware;
